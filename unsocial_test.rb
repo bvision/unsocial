02198069f7c3d0955cd1062c8ddb6e3c515ae27d
Adrian Mugnolo <adrian@mugnolo.com>
2012-12-25 23:30:21 -0300

    Make tests simpler

--- unsocial_test.rb
+++ unsocial_test.rb
@@ -20,18 +20,15 @@ class UnsocialTest < Test::Unit::TestCase
   def test_not_found_should_respond_with_404
     get '/xxx'
     assert_equal 404, last_response.status
-    assert_match(/Not Found/, last_response.body)
   end
 
   def test_robots_txt_should_respond_with_200
     get '/robots.txt'
     assert_equal 200, last_response.status
-    assert_equal '', last_response.body
   end
 
   def test_google_verification_should_respond_with_200
     get '/google3937f044695dbc67.html'
     assert_equal 200, last_response.status
-    assert_match(/google-site-verification/, last_response.body)
   end
 end
